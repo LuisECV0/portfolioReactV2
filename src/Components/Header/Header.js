cf864def3a4ff333fcea322de9d29b2c162259ac
Luis <141294557+LuisECV0@users.noreply.github.com>
2023-12-22 23:59:17 -0500

    implementacion de bootstrap para el navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,41 +1,37 @@
 import React, { useState } from 'react';
-import './Header.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
 const Header = () => {
-  const [burgerClass, setBurgerClass] = useState("burger-bar unclicked");
-  const [menuClass, setMenuClass] = useState("menu hidden");
-  const [isMenuClicked, setIsMenuClicked] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
-  const updateMenu = () => {
-    if (!isMenuClicked) {
-      setBurgerClass("burger-bar clicked");
-      setMenuClass("menu visible");
-    } else {
-      setBurgerClass("burger-bar unclicked");
-      setMenuClass("menu hidden");
-    }
-    setIsMenuClicked(!isMenuClicked);
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
   };
 
   return (
-    <header className="header">
-      <div id="logo">
-        <a href="/">LuisDev</a>
-      </div>
-      <nav>
-        <div className='burger-menu' onClick={updateMenu}>
-          <div className={burgerClass}></div>
-          <div className={burgerClass}></div>
-          <div className={burgerClass}></div>
-        </div>
-      </nav>
-
-      <div className={menuClass}>
-        <a href="#/qw" className="menu-item">Skills</a>
-        <a href="#/qw" className="menu-item">Contact</a>
-        <a href="#/qw" className="menu-item">Projects</a>
-      </div>
-    </header>
+    <Navbar bg="dark" variant="dark" expand="md">
+      <Container>
+        <Navbar.Brand>NavBar <span>Responsive</span></Navbar.Brand>
+        <Navbar.Toggle onClick={toggleMenu} aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav" className={`justify-content-end ${isMenuOpen ? 'show' : ''}`}>
+          <Nav>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/">Acerca de mí</Nav.Link>
+            <Nav.Link href="/">Habilidades</Nav.Link>
+            <Nav.Link href="/">Proyectos</Nav.Link>
+            <Nav.Link href="/">Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Button className="burger-button" variant="outline-light" onClick={toggleMenu}>
+          <span className={`burger ${isMenuOpen ? 'active' : ''}`} id="hamburger-1"></span>
+          <span className={`burger ${isMenuOpen ? 'active' : ''}`} id="hamburger-2"></span>
+          <span className={`burger ${isMenuOpen ? 'active' : ''}`} id="hamburger-3"></span>
+        </Button>
+      </Container>
+    </Navbar>
   );
 };
 
